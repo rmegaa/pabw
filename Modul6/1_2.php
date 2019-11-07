bbe9b24789b9a5a574f5f8304dda1a2da20868e5
Ratu Mega Aprillia <ratumega25@gmail.com>
2019-11-07 23:56:15 +0700

    Modul8

--- Modul6/1_2.php
+++ Modul6/1_2.php
@@ -3,7 +3,7 @@ $a = 3;
 $b = 7;
 $a += 5; //$a = $a + 5;
 $b =($c =11) + 3;
-echo "Nilai variabel a adalah = $a <br/>";
-echo "Nilai variabel b adalah = $b <br/>";
-echo "Nilai variabel c adalah = $c <br/>";
+echo "Nilai variabel a adalah = $a \n";
+echo "Nilai variabel b adalah = $b \n";
+echo "Nilai variabel c adalah = $c \n";
 ?>
---
+++ Modul6/1_3.php
@@ -0,0 +1,12 @@
+<?php
+$a = 5;
+$b = 7;
+echo "a = $a \n";
+echo "b = $b \n";
+if ($a == $b)
+    echo "$a sama dengan $b";
+elseif ($a > $b)
+    echo "$a lebih besar daripada $b";
+else 
+    echo  "$a lebih kecil daripada $b";
+?>
